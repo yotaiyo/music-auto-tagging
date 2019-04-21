625fa40c162053ed82c07620ecdcd105236c7f73
yotaiyo <yota071218@gmail.com>
2019-04-22 06:28:31 +0900

    rename create_features_dir.py

--- create_features_norm_dir.py
+++ create_features_norm_dir.py
@@ -1,13 +1,16 @@
+import sys
 import pandas as pd
 import random
 import os
 import pickle
 
-def create_features_norm_dir(split_name, df):
+def create_features_norm_dir(df, split_name):
+    print('start create %s features_norm_dir' %split_name)
     features_dir = []
     labels = []
     labels_column = df.iloc[:,1:51].columns.values.tolist()
     for index in df.index:
+        print('index', index)
         split = df.split[index]
         clip_id = df.clip_id[index]
         if split == split_name:
@@ -21,13 +24,18 @@ def create_features_norm_dir(split_name, df):
     return pd.concat([df_features_dir,df_labels],axis=1)
 
 if __name__ == '__main__':
-    df = pd.read_csv('annotations_final_top_50_tag.csv')
+    if len(sys.argv) == 2:
+        input_path = sys.argv[1]
+        df = pd.read_csv(input_path)
 
-    if not os.path.exists('features_dir'):
-        os.mkdir('features_dir')
+        if not os.path.exists('features_dir'):
+            os.mkdir('features_dir')
 
-    df_train_dir = create_features_norm_dir('train', df)
-    df_train_dir.to_csv('features_dir/feature_train_dir.csv',index=None)
+        df_train_dir = create_features_norm_dir(df, 'train')
+        df_train_dir.to_csv('features_dir/feature_train_dir.csv',index=None)
 
-    df_val_dir = create_features_norm_dir('val', df)
-    df_val_dir.to_csv('features_dir/feature_val_dir.csv',index=None)
+        df_val_dir = create_features_norm_dir(df, 'val')
+        df_val_dir.to_csv('features_dir/feature_val_dir.csv',index=None)
+    
+    else:
+        print('usage: python create_features_norm_dir.py input_path:annotations_final_top_50_tag.csv')
